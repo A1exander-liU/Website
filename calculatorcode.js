fea785ce6b331c45729193a03c784834261a8b3e
A1exander-liU <alexanderliu2016@gmail.com>
2022-02-28 19:34:47 -0800

    added and linked lab 6

---
+++ calculatorcode.js
@@ -0,0 +1,71 @@
+function add(){
+    a = parseFloat(jQuery('#num1').val())
+    b = parseFloat(jQuery('#num2').val())
+    sum = a + b
+    result = a + " + " + b + " = " + sum
+    jQuery('#result').html(result)
+    recent = result
+    old = jQuery('#history').html()
+    jQuery('#history').html(old + "<br><span class='blue'>" + recent + "</span>")
+}
+
+function sub(){
+    a = jQuery('#num1').val()
+    b = jQuery('#num2').val()
+    difference =  a - b
+    result = a + " - " + b + " = " + difference
+    jQuery('#result').html(result)
+    recent = result
+    old = jQuery('#history').html()
+    jQuery('#history').html(old + "<br><span class='red'>" + recent + "</span>")
+}
+
+function mult(){
+    a = jQuery('#num1').val()
+    b = jQuery('#num2').val()
+    product = a * b
+    result = a + " * " + b + " = " + product
+    jQuery('#result').html(result)
+    recent = result
+    old = jQuery('#history').html()
+    jQuery('#history').html(old + "<br><span class='green'>" + recent + "</span>")
+}
+
+function div(){
+    a = jQuery('#num1').val()
+    b = jQuery('#num2').val()
+    quotient =  a / b
+    result = a + " / " + b + " = " + quotient
+    jQuery('#result').html(result)
+    recent = result
+    old = jQuery('#history').html()
+    jQuery('#history').html(old + "<br><span class='yellow'>" + recent + "</span>")
+}
+
+function font_increase(){
+    calcs = jQuery('#history span').text()
+    current_size = parseInt(jQuery('#history span').css('font-size'))
+    current_size = current_size + 10
+    jQuery('#history span').css('font-size', current_size)
+    console.log(current_size)
+    console.log()
+}
+
+function font_decrease(){
+    calcs = jQuery('#history span').text()
+    current_size = parseInt(jQuery('#history span').css('font-size'))
+    current_size = current_size - 10
+    jQuery('#history span').css('font-size', current_size)
+}
+
+
+function setup(){
+    jQuery('#add').click(add)
+    jQuery('#subtract').click(sub)
+    jQuery('#multiply').click(mult)
+    jQuery('#divide').click(div)
+    jQuery('#font-up').click(font_increase)
+    jQuery('#font-down').click(font_decrease)
+}
+
+jQuery(document).ready(setup)
